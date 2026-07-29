63f2283d5bdfd076840cc09f6e14f41b31ad2150
REDRAGON <148379059+REDRAGO14@users.noreply.github.com>
2026-07-29 13:18:34 -0700

    learned how to stop a loop using Break

---
+++ part5/levelUpChallenges.js
@@ -0,0 +1,15 @@
+//CHALLENGE NUMBER 1 
+/* let teaCollection = ["green tea", "black tea", "oolang tea", "chai"]
+let selectedTeas = []
+
+for( let i = 0; i < teaCollection.length; i++){
+    if(teaCollection[i] != "oolang tea"){
+        selectedTeas.push(teaCollection[i])
+    }else{
+        break;
+    }
+}
+console.log(selectedTeas);
+ */
+
+
